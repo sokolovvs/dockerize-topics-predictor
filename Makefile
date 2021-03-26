d8df6109ac0827faee2e176e95518d58a2cd7573
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-03-26 11:44:05 +0600

    added installation script

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 start:
+		bash ./installation.sh
 		docker-compose up --build --remove-orphans -d
 stop:
 		docker ps -aq --filter="name=dockerizetopicspredictor" | xargs -r docker stop
---
+++ installation.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+rootPath=`pwd`
+pathToSrc="${rootPath}/src"
+
+echo "try copy .env from dockerize project"
+cp .env.example .env
+
+# INSTALL TPS
+pathToTps="${rootPath}/src/tps"
+
+if [[ ! -d "$pathToTps" ]]
+then
+  echo "try clone the sokolovvs/tps repository"
+  cd "$pathToSrc" && git clone git@github.com:sokolovvs/tps.git
+fi
+
+echo "try update the sokolovvs/tps repository"
+cd "$pathToTps" && git pull
+
+echo "try copy .env from tps project"
+cd "$pathToTps" && cp .env.example .env
+
+
+
+# INSTALL app-for-purchasing-service
+pathToPs="${rootPath}/src/app-for-purchasing-service"
+
+if [[ ! -d "$pathToPs" ]]
+then
+  echo "try clone the sokolovvs/app-for-purchasing-service repository"
+  cd "$pathToSrc" && git clone git@github.com:sokolovvs/app-for-purchasing-service.git
+fi
+
+echo "try update the sokolovvs/app-for-purchasing-service repository"
+cd "$pathToPs" && git pull
+
+echo "try copy .env from app-for-purchasing-service project"
+cd "$pathToPs" && cp .env.example .env
