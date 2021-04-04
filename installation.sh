8ff53712ab5d2e1e4ff743d9404d6e7d4ee41b2f
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-04 09:12:28 +0600

    improved copying .env params

--- installation.sh
+++ installation.sh
@@ -1,19 +1,56 @@
 #!/bin/bash
 
-rootPath=`pwd`
+# read dot env file to hashmap $tmpHash
+# param $1 - path to dot env file
+function readDotEnvIntoTmpHash() {
+  local pathToDotEnv=$1
+
+  for line in $(egrep -v '^#' $pathToDotEnv | xargs); do
+    IFS="=" read -ra STR_ARRAY <<<"$line"
+    local k=${STR_ARRAY[0]}
+    local v=${STR_ARRAY[1]}
+    tmpHash["$k"]="$v"
+  done
+}
+
+# cp env.example .env with saving defined values from .env
+# param $1 - path to dot env file
+function updateDotEnv() {
+  declare -A tmpHash
+
+  pathToEnv=$1
+  readDotEnvIntoTmpHash "$pathToEnv.example"
+  readDotEnvIntoTmpHash "$pathToEnv"
+  writeDotEnv "$pathToEnv"
+
+  unset tmpHash
+}
+
+# write hashmap $tmpHash to dot env file
+# param $1 - path to dot env file
+function writeDotEnv() {
+  pathToEnv=$1
+
+  cat /dev/null >"$pathToEnv"
+
+  for k in "${!tmpHash[@]}"; do
+    echo "$k=${tmpHash[$k]}" >>"$pathToEnv"
+  done
+}
+
+rootPath=$(pwd)
 pathToSrc="${rootPath}/src"
 
 echo "try update dockerize project"
 git pull
 
 echo "try copy .env from dockerize project"
-cp .env.example .env
+updateDotEnv "$rootPath/.env"
 
 # INSTALL TPS
 pathToTps="${rootPath}/src/tps"
 
-if [[ ! -d "$pathToTps" ]]
-then
+if [[ ! -d "$pathToTps" ]]; then
   echo "try clone the sokolovvs/tps repository"
   cd "$pathToSrc" && git clone git@github.com:sokolovvs/tps.git
 fi
@@ -22,15 +59,12 @@ echo "try update the sokolovvs/tps repository"
 cd "$pathToTps" && git pull
 
 echo "try copy .env from tps project"
-cd "$pathToTps" && cp .env.example .env
-
-
+updateDotEnv "$pathToTps/.env"
 
 # INSTALL app-for-purchasing-service
 pathToPs="${rootPath}/src/app-for-purchasing-service"
 
-if [[ ! -d "$pathToPs" ]]
-then
+if [[ ! -d "$pathToPs" ]]; then
   echo "try clone the sokolovvs/app-for-purchasing-service repository"
   cd "$pathToSrc" && git clone git@github.com:sokolovvs/app-for-purchasing-service.git
 fi
@@ -39,4 +73,4 @@ echo "try update the sokolovvs/app-for-purchasing-service repository"
 cd "$pathToPs" && git pull
 
 echo "try copy .env from app-for-purchasing-service project"
-cd "$pathToPs" && cp .env.example .env
+updateDotEnv "$pathToPs/.env"
