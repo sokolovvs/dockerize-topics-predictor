89948ad44d5cd27b13f9567b638c253d5a664974
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-06 16:36:27 +0600

    updated installation.sh

--- installation.sh
+++ installation.sh
@@ -41,36 +41,36 @@ function writeDotEnv() {
 rootPath=$(pwd)
 pathToSrc="${rootPath}/src"
 
-echo "try update dockerize project"
+echo "trying to update dockerize project"
 git pull
 
-echo "try copy .env from dockerize project"
+echo "trying to copy .env from dockerize project"
 updateDotEnv "$rootPath/.env"
 
 # INSTALL TPS
 pathToTps="${rootPath}/src/tps"
 
 if [[ ! -d "$pathToTps" ]]; then
-  echo "try clone the sokolovvs/tps repository"
+  echo "trying to clone the sokolovvs/tps repository"
   cd "$pathToSrc" && git clone git@github.com:sokolovvs/tps.git
 fi
 
-echo "try update the sokolovvs/tps repository"
+echo "trying to update the sokolovvs/tps repository"
 cd "$pathToTps" && git pull
 
-echo "try copy .env from tps project"
+echo "trying to copy .env from tps project"
 updateDotEnv "$pathToTps/.env"
 
 # INSTALL app-for-purchasing-service
 pathToPs="${rootPath}/src/app-for-purchasing-service"
 
 if [[ ! -d "$pathToPs" ]]; then
-  echo "try clone the sokolovvs/app-for-purchasing-service repository"
+  echo "trying to clone the sokolovvs/app-for-purchasing-service repository"
   cd "$pathToSrc" && git clone git@github.com:sokolovvs/app-for-purchasing-service.git
 fi
 
-echo "try update the sokolovvs/app-for-purchasing-service repository"
+echo "trying to update the sokolovvs/app-for-purchasing-service repository"
 cd "$pathToPs" && git pull
 
-echo "try copy .env from app-for-purchasing-service project"
+echo "trying to copy .env from app-for-purchasing-service project"
 updateDotEnv "$pathToPs/.env"
