220e98de05fceca5dc12f6e77b9bb8dce6dd11ad
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-08-28 19:14:32 +0700

    move update dot env to other file and use via source

--- installation.sh
+++ installation.sh
@@ -1,42 +1,6 @@
 #!/bin/bash
 
-# read dot env file to hashmap $tmpHash
-# param $1 - path to dot env file
-function readDotEnvIntoTmpHash() {
-  local pathToDotEnv=$1
-
-  for line in $(egrep -v '^#' $pathToDotEnv | xargs); do
-    IFS="=" read -ra STR_ARRAY <<<"$line"
-    local k=${STR_ARRAY[0]}
-    local v=${STR_ARRAY[1]}
-    tmpHash["$k"]="$v"
-  done
-}
-
-# cp env.example .env with saving defined values from .env
-# param $1 - path to dot env file
-function updateDotEnv() {
-  declare -A tmpHash
-
-  pathToEnv=$1
-  readDotEnvIntoTmpHash "$pathToEnv.example"
-  readDotEnvIntoTmpHash "$pathToEnv"
-  writeDotEnv "$pathToEnv"
-
-  unset tmpHash
-}
-
-# write hashmap $tmpHash to dot env file
-# param $1 - path to dot env file
-function writeDotEnv() {
-  pathToEnv=$1
-
-  cat /dev/null >"$pathToEnv"
-
-  for k in "${!tmpHash[@]}"; do
-    echo "$k=${tmpHash[$k]}" >>"$pathToEnv"
-  done
-}
+source ./update-env.sh
 
 rootPath=$(pwd)
 pathToSrc="${rootPath}/src"
